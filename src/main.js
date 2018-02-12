cce186fe1d5939c7c61f8d27cf8bf67b37e03eca
qiyang <qi.yang@tendcloud.com>
2018-02-12 16:37:53 +0800

    feat(main):增加main配置

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+/**
+ * yangqi by 2018/02/12
+ */
+
+import 'iview/dist/styles/iview.css';
 import Vue from 'vue';
+import iView from 'iview';
 import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+Vue.use(iView);
+
 
 /* eslint-disable no-new */
 new Vue({
