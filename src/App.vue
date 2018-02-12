f659895ee311d4d3bba4272523a7220ad1e2578c
qiyang <qi.yang@tendcloud.com>
2018-02-12 22:58:03 +0800

    fix(app):修改app文件

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,25 @@
 <template>
   <div id="app">
-    <router-view/>
+    <div class="mainWrap">
+      <main-left-nav></main-left-nav>
+      <div id="mainContent">
+        <router-view></router-view>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App',
+  import mainLeftNav from './components/MenuLeftNav.vue';
+
+  export default {
+    name: 'App',
+    components: {
+      mainLeftNav,
+    },
 };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
