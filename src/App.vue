b8f5b5e09a5bd4235b563bd69a3fbb91c97448af
qiyang <qi.yang@tendcloud.com>
2018-02-12 23:25:19 +0800

    feat(头部导航):增加头部导航

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <main-top-nav></main-top-nav>
+
     <div class="mainWrap">
       <main-left-nav></main-left-nav>
       <div id="mainContent">
@@ -10,16 +12,19 @@
 </template>
 
 <script>
+  import mainTopNav from './components/MainHeaderNav.vue';
   import mainLeftNav from './components/MenuLeftNav.vue';
 
   export default {
     name: 'App',
     components: {
+      mainTopNav,
       mainLeftNav,
     },
 };
 </script>
 
 <style>
+  @import "assets/styles/reset.css";
 
 </style>
