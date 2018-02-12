dc683531e89eb9c629c082e4979167d0678c41aa
qiyang <qi.yang@tendcloud.com>
2018-02-12 17:30:09 +0800

    feat(路由):增加登陆，注册路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,37 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      path: 'signup',
+      name: 'userSignUp',
+      component(resolve) {
+        require.ensure(['../views/PageSignUp.vue'], () => {
+          // eslint-disable-next-line
+          resolve(require('../views/PageSignUp.vue'));
+        });
+      },
+      meta: {
+        title: '用户注册',
+      },
+    },
+    {
+      path: 'login',
+      name: 'userLogIn',
+      component(resolve) {
+        require.ensure(['../views/PageSignIn.vue'], () => {
+          // eslint-disable-next-line
+          resolve(require('../views/PageSignIn.vue'));
+        });
+      },
+      meta: {
+        title: '用户登陆',
+      },
     },
   ],
 });
+
+export default router;
