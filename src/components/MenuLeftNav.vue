e02de52d8ac78348eeda39e9553f7ddeafe732d8
qiyang <qi.yang@tendcloud.com>
2018-02-12 18:09:01 +0800

    feat(侧边栏):增加侧边栏配置

--- src/components/MenuLeftNav.vue
+++ src/components/MenuLeftNav.vue
@@ -1,10 +1,25 @@
 <template>
-  <div></div>
+  <div class="left-menu">
+    <ul>
+      <li class="trans"
+          v-for="item in navList"
+          :class="{'active': $route.path.split('/')[1] === item.name}"
+          @click="$router.push(item.target)">
+        <a href="javascript:void(0);">
+          {{item.title}}
+        </a>
+      </li>
+    </ul>
+  </div>
 </template>
-
 <script>
+  import navListData from '../config/menu-left-nav.js';
+
+  export default {
+    data() {
+      return {
+        navList: navListData,
+      };
+    },
+  };
 </script>
-
-<style scoped>
-
-</style>
---
+++ src/config/menu-left-nav.js
@@ -0,0 +1,34 @@
+// 边栏 数据
+const navConfig = [
+  {
+    name: 'overview',
+    target: '/overview',
+    title: '项目概览',
+    // isActive: false,
+    iconClass: 'icon-summary',
+    children: [{}],
+  },
+  {
+    name: 'activity',
+    target: '/activity',
+    title: '营销活动',
+    iconClass: 'icon-ADactive',
+    children: [{}],
+  },
+  {
+    name: 'defend',
+    target: '/defend',
+    title: '作弊防护',
+    iconClass: 'icon-defend',
+    children: [{}],
+  },
+  {
+    name: 'setting',
+    target: '/setting',
+    title: '项目设置',
+    iconClass: 'icon-setting',
+    children: [{}],
+  },
+];
+
+export default navConfig;
