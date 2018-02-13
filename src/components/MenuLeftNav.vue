a4e0107ddd1a08b159e3d50ad05b127aa20204c8
qiyang <qi.yang@tendcloud.com>
2018-02-13 20:38:18 +0800

    fix(侧边栏):修改侧边栏样式

--- src/components/MenuLeftNav.vue
+++ src/components/MenuLeftNav.vue
@@ -6,7 +6,7 @@
           :class="{'active': $route.path.split('/')[1] === item.name}"
           @click="$router.push(item.target)">
         <a href="javascript:void(0);">
-          {{item.title}}
+          <i style="margin-left: 35px">{{item.title}}</i>
         </a>
       </li>
     </ul>
@@ -23,3 +23,27 @@
     },
   };
 </script>
+<style>
+  .left-menu {
+    position: fixed;
+    top: 90px;
+    left: 0;
+    bottom: 0;
+    background: #1A1B1E;
+    width: 120px;
+    z-index: 5;
+  }
+  .left-menu ul li a {
+    color: #a8adbb;
+    height: 62px;
+    line-height: 62px;
+    display: block;
+    border-left: 4px solid transparent;
+  }
+  .left-menu ul li.active a {
+    border-left-color: #D15151;
+    cursor: pointer;
+    color: #fff;
+    background: #0A0A0D;
+  }
+</style>
