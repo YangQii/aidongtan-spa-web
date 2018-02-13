e83707c888d7f01364feee13340ed08d07214bb6
qiyang <qi.yang@tendcloud.com>
2018-02-13 23:05:38 +0800

    fix(总栏):修改样式

--- src/views/PageOverview.vue
+++ src/views/PageOverview.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="content">
-
+    <div class="content_info">
+      <div>
+        <span>总盈利金额:</span>
+        <span class="content_info-span">健身房点击量: 8955000 次</span>
+      </div>
+      <div style="border-bottom: 1px solid #DEDEDE;padding-bottom: 50px">
+        <span class="content_info-span-m">54886000<i>元</i></span>
+        <span class="content_info-span">健身房点击量: 8955000 次</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,6 +24,24 @@
     height: 800px;
     margin: 120px 31px 150px 150px;
     background: #FFFFFF;
+    padding-top: 42px;
+    padding-left: 55px;
+    padding-right: 75px;
   }
-
+  .content_info {
+    font-size:24px;
+    color: #333333;
+    font-family:MicrosoftYaHei;
+  }
+  .content_info-span-m {
+    font-size:36px;
+    color: #FF4D55;
+  }
+  .content_info-span-m i {
+    font-size: 14px;
+  }
+  .content_info-span {
+    font-size: 18px;
+    float: right;
+  }
 </style>
