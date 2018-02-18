eda5ba3084da507aba2a31839462b1ae6e398ea9
qiyang <qi.yang@tendcloud.com>
2018-02-18 22:37:11 +0800

    fix(place):修改style2

--- src/views/PagePlaceEq.vue
+++ src/views/PagePlaceEq.vue
@@ -1,10 +1,59 @@
 <template>
-
+  <div>
+    <div>
+      <Input v-model="value1" size="large" placeholder="large size"></Input>
+    </div>
+    <Table :columns="columns1" :data="data1"></Table>
+  </div>
 </template>
 
 <script>
   export default {
-    name: "page-place-eq"
+    name: "page-place-eq",
+    data () {
+      return {
+        columns1: [
+          {
+            title: 'Name',
+            key: 'name'
+          },
+          {
+            title: 'Age',
+            key: 'age'
+          },
+          {
+            title: 'Address',
+            key: 'address'
+          }
+        ],
+        data1: [
+          {
+            name: 'John Brown',
+            age: 18,
+            address: 'New York No. 1 Lake Park',
+            date: '2016-10-03'
+          },
+          {
+            name: 'Jim Green',
+            age: 24,
+            address: 'London No. 1 Lake Park',
+            date: '2016-10-01'
+          },
+          {
+            name: 'Joe Black',
+            age: 30,
+            address: 'Sydney No. 1 Lake Park',
+            date: '2016-10-02'
+          },
+          {
+            name: 'Jon Snow',
+            age: 26,
+            address: 'Ottawa No. 2 Lake Park',
+            date: '2016-10-04'
+          }
+        ]
+      }
+    }
   }
 </script>
 
