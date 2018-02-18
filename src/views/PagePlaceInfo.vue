68c9785fd65aa17542f44e31ff1a20328cbc796e
qiyang <qi.yang@tendcloud.com>
2018-02-18 20:04:34 +0800

    feat(场馆):增加场馆信息info

--- src/views/PagePlaceInfo.vue
+++ src/views/PagePlaceInfo.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="index-content_info"></div>
+  <div class="place-content">
+    <div class="place-content_info">
+      <div class="place-content_info_left"></div>
+      <div class="place-content_info_right"></div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +14,23 @@
 </script>
 
 <style scoped>
-
+  .place-content {
+    padding: 43px 13px 10px 34px;
+    background: #FFFFFF;
+  }
+  .place-content_info {
+    height: 420px;
+    background: #3c3f43;
+    width: 100%;
+    margin: -20px 0 0 -10px;
+  }
+  .place-content_info_left {
+    width: 420px;
+    height: 100%;
+    background: #c7ccd3;
+  }
+  .place-content_info_right {
+    width: 100%;
+    height: ;
+  }
 </style>
